804702897b5ebc64f188a0dec54f334fedd3547f
Kevin Aryel <kevinaryel.dev@gmail.com>
2022-10-05 23:48:49 -0300

    feat: Atividade stream realizada

--- modulo 1/exercicio-stream/src/Stream.java
+++ modulo 1/exercicio-stream/src/Stream.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Stream {
     public static void main(String[] args) {
@@ -14,27 +14,133 @@ public class Stream {
         lista.add(new Pessoa(++i, "Gustavo", 18000, "Diretor"));
 
         //1- listar todas as pessoas
-//        lista.stream()
-//                .forEach();
+
+        System.out.println("\n--------------------- Resposta Atividade 1 ---------------------\n");
+
+        lista.stream().
+                forEach(pessoa-> System.out.println(pessoa));
 
         //2- filtrar todas as pessoas com salario maior do que 5 mil (filter)
 
+        System.out.println("\n--------------------- Resposta Atividade 2 ---------------------\n");
+
+        List<Pessoa> exercicioStream2 = lista.stream()
+                .filter(pessoa -> pessoa.getSalario() > 5000).toList();
+
+        System.out.println(exercicioStream2);
+
         //3- filtrar todas as pessoas que são desenvolvedoras e organizar por salário crescente (filter, sorted)
+
+        System.out.println("\n--------------------- Resposta Atividade 3 ---------------------\n");
+
+        List<Pessoa> exercicioStream3 = lista.stream()
+                .filter(pessoa -> pessoa.getCargo() == "Desenvolvedor").sorted(Comparator.comparing(Pessoa::getSalario)).toList();
+
+        System.out.println(exercicioStream3);
 
         //4- fazer a média salarial de todos
 
+        System.out.println("\n--------------------- Resposta Atividade 4 ---------------------\n");
+
+        Double exercicioStream4 = lista.stream()
+                .mapToDouble(Pessoa::getSalario)
+                .average()
+                .getAsDouble();
+
+        System.out.println(exercicioStream4);
+
         //5- verificar na lista (utilizando o método anyMatch) se tem alguém que ganha mais do que 20 mil
+
+        System.out.println("\n--------------------- Resposta Atividade 5 ---------------------\n");
+
+        boolean exercicioStream5 = lista.stream()
+                .anyMatch(pessoa -> pessoa.getSalario() >20000);
+
+        System.out.println(exercicioStream5);
 
         //6- retornar uma lista de todos os ids das pessoas
 
-        //7- criar uma nova classe Salario com ID e Salário, utilizando a função "map" do stream, retornar uma lista desse novo objeto
+        System.out.println("\n--------------------- Resposta Atividade 6 ---------------------\n");
+
+        List<Integer> exercicioStream6 = lista.stream()
+                .map(pessoa -> pessoa.getId()).toList();
+
+        System.out.println(exercicioStream6);
+
+        //7- criar uma nova classe Salario com ID e Salário, utilizando a função "map" do stream,
+        // retornar uma lista desse novo objeto
+
+        System.out.println("\n--------------------- Resposta Atividade 7 ---------------------\n");
+
+        List<Salario> exercicioStream7 = lista.stream()
+                .map(pessoa-> new Salario(pessoa.getId(), pessoa.getSalario())).toList();
+
+        System.out.println(exercicioStream7);
 
         //8- retornar um HashMap (estrutura de dados, e não uma função map) contendo os ids e os nomes dos colaboradores
 
+        System.out.println("\n--------------------- Resposta Atividade 8 ---------------------\n");
+
+        Map<Integer,String> exercicioStream8 = lista.stream()
+                .collect(Collectors.toMap(Pessoa::getId, Pessoa::getNome));
+
+        System.out.println(exercicioStream8);
+
         //9- com o mapa da 8, retornar o nome com o id=2
+
+        System.out.println("\n--------------------- Resposta Atividade 9 ---------------------\n");
+
+        System.out.println(exercicioStream8.get(2));
 
         //10- verificar se tem alguém que contenha o nome "Paulo" (containsignorecase) na lista e retornar o primeiro elemento que encontrar (findFirst).
         //    Imprimir o nome e salário dessa pessoa
+
+        System.out.println("\n--------------------- Resposta Atividade 10 ---------------------\n");
+
+           Optional<Pessoa> exercicioStream9 = lista.stream()
+                   .filter(pessoa -> pessoa.getNome().contains("Paulo"))
+                   .findFirst();
+           if (exercicioStream9.isPresent()){
+               System.out.println(exercicioStream9.get().getNome()+", "+exercicioStream9.get().getSalario());
+        }
+    }
+
+
+    static class Salario{
+
+        private int id;
+        private double salario;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public double getSalario() {
+            return salario;
+        }
+
+        public void setSalario(double salario) {
+            this.salario = salario;
+        }
+
+        public Salario(int id, double salario){
+            this.id = id;
+            this.salario = salario;
+        }
+
+
+        @Override
+        public String toString(){
+            return "Salario {" +
+                    "id = "+id+
+                    ", salario = "+salario+
+                    '}';
+        };
+
     }
 
     static class Pessoa {
@@ -53,10 +159,10 @@ public class Stream {
         @Override
         public String toString(){
           return "Pessoa {" +
-                  "id="+id+
-                  ", nome ="+nome+
-                  ", salario="+salario+
-                  ", cargo ="+cargo;
+                  "id = "+id+
+                  ", nome = "+nome+
+                  ", salario = "+salario+
+                  ", cargo = "+cargo+'}';
         };
 
         public int getId() {
