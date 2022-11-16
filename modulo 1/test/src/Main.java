ed7dc0c03f3f9cf68f2c9d9a095a3b1abdb9eea0
Kevin <kevinaryel.dev@gmail.com>
2022-11-16 00:31:52 -0300

    Limpando repositorio

--- modulo 1/test/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int x = 1;
-        int y = 5;
-        int w = 200 / (5 -  1 ) + 2;
-        System.out.println(w);
-       ;
-    }
-}
